2882287c0dea28236145c948191a4071f24acbea
Jean-Baptiste Kaiser <jb@marmelab.com>
2025-05-19 13:34:42 +0200

    make navigation bar generic

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,6 +1,4 @@
 import {
-  NavigationMenuContent,
-  NavigationMenuTrigger,
   navigationMenuTriggerStyle,
   NavigationMenu,
   NavigationMenuItem,
@@ -8,73 +6,76 @@ import {
   NavigationMenuList,
 } from "@/components/ui/navigation-menu";
 import { cn } from "@/lib/utils";
-import { useGetList } from "ra-core";
+import {
+  useCreatePath,
+  useGetResourceLabel,
+  useHasDashboard,
+  useResourceDefinitions,
+  useTranslate,
+} from "ra-core";
 import { Link } from "react-router-dom";
 
 export const NavigationBar = () => {
-  const { data: categories = [] } = useGetList("categories", {
-    pagination: { page: 1, perPage: 1000 },
-    sort: { field: "name", order: "ASC" },
-  });
+  const hasDashboard = useHasDashboard();
+  const resources = useResourceDefinitions();
   return (
     <NavigationMenu>
       <NavigationMenuList>
-        <NavigationMenuItem>
-          <NavigationMenuLink asChild>
-            <Link
-              to="/"
-              className={cn(
-                navigationMenuTriggerStyle(),
-                "font-normal hover:bg-background"
-              )}
-            >
-              React-Admin Shadcn Demo
-            </Link>
-          </NavigationMenuLink>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger className="font-normal hover:bg-background">
-            Products
-          </NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-              <li>
-                <NavigationMenuLink asChild>
-                  <Link to="/products">All products</Link>
-                </NavigationMenuLink>
-              </li>
-              {categories.map((category) => (
-                <li key={category.id}>
-                  <NavigationMenuLink asChild>
-                    <Link
-                      to={`/products?filter=${encodeURIComponent(
-                        JSON.stringify({
-                          category_id: category.id,
-                        })
-                      )}`}
-                    >
-                      {category.name}
-                    </Link>
-                  </NavigationMenuLink>
-                </li>
-              ))}
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuLink asChild>
-            <Link
-              to="/categories"
-              className={cn(
-                navigationMenuTriggerStyle(),
-                "font-normal hover:bg-background"
-              )}
-            >
-              Categories
-            </Link>
-          </NavigationMenuLink>
-        </NavigationMenuItem>
+        {hasDashboard ? <DashboardMenuItem /> : null}
+        {Object.keys(resources)
+          .filter((name) => resources[name].hasList)
+          .map((name) => (
+            <ResourceMenuItem key={name} name={name} />
+          ))}
       </NavigationMenuList>
     </NavigationMenu>
+  );
+};
+
+const DashboardMenuItem = () => {
+  const translate = useTranslate();
+  const label = translate("ra.page.dashboard", {
+    _: "Dashboard",
+  });
+  return (
+    <NavigationMenuItem>
+      <NavigationMenuLink asChild>
+        <Link
+          to="/"
+          className={cn(
+            navigationMenuTriggerStyle(),
+            "font-normal hover:bg-background"
+          )}
+        >
+          {label}
+        </Link>
+      </NavigationMenuLink>
+    </NavigationMenuItem>
+  );
+};
+
+const ResourceMenuItem = ({ name }: { name: string }) => {
+  const resources = useResourceDefinitions();
+  const getResourceLabel = useGetResourceLabel();
+  const createPath = useCreatePath();
+  if (!resources || !resources[name]) return null;
+  return (
+    <NavigationMenuItem>
+      <NavigationMenuLink asChild>
+        <Link
+          to={createPath({
+            resource: name,
+            type: "list",
+          })}
+          state={{ _scrollToTop: true }}
+          className={cn(
+            navigationMenuTriggerStyle(),
+            "font-normal hover:bg-background"
+          )}
+        >
+          {getResourceLabel(name, 2)}
+        </Link>
+      </NavigationMenuLink>
+    </NavigationMenuItem>
   );
 };
