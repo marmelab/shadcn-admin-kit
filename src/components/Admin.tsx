77374b1f3edffd8981589cc4b8198d949328b377
Jean-Baptiste Kaiser <jb@marmelab.com>
2025-05-19 11:53:45 +0200

    add theme toggle

--- src/components/Admin.tsx
+++ src/components/Admin.tsx
@@ -2,15 +2,18 @@ import { CoreAdmin, CoreAdminProps } from "ra-core";
 import { i18nProvider } from "@/lib/i18nProvider";
 import { Layout } from "@/components/Layout";
 import { LoginPage } from "@/components/LoginPage";
+import { ThemeProvider } from "@/components/ThemeProvider";
 
 export const Admin = (props: CoreAdminProps) => {
   return (
-    <CoreAdmin
-      i18nProvider={i18nProvider}
-      layout={Layout}
-      loginPage={LoginPage}
-      {...props}
-    />
+    <ThemeProvider>
+      <CoreAdmin
+        i18nProvider={i18nProvider}
+        layout={Layout}
+        loginPage={LoginPage}
+        {...props}
+      />
+    </ThemeProvider>
   );
-}
+};
 
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,14 +1,18 @@
 import { CoreLayoutProps } from "ra-core";
 import { NavigationBar } from "@/components/NavigationBar";
 import { UserMenu } from "@/components/UserMenu";
+import { ThemeModeToggle } from "@/components/ThemeModeToggle";
 
 export const Layout = (props: CoreLayoutProps) => {
   return (
     <div className="min-h-screen flex flex-col p-2">
-      <div className="border-b grow-0">
+      <div className="border-b grow-0 pb-2">
         <div className="flex pr-4 w-full justify-between">
           <NavigationBar />
-          <UserMenu />
+          <div className="flex items-center gap-2">
+            <ThemeModeToggle />
+            <UserMenu />
+          </div>
         </div>
       </div>
       <div className="grow mt-8">{props.children}</div>
