4153736d1c959bddfa7b84369431f8cffce42aac
Jean-Baptiste Kaiser <jb@marmelab.com>
2025-05-19 18:24:52 +0200

    add ShowGuesser

---
+++ src/components/ShowGuesser.tsx
@@ -0,0 +1,123 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { ReactNode, useEffect, useState } from "react";
+import {
+  ShowBase,
+  InferredElement,
+  getElementsFromRecords,
+  useResourceContext,
+  useShowContext,
+  InferredTypeMap,
+} from "ra-core";
+import { capitalize, singularize } from "inflection";
+import { ShowView } from "@/components/Show";
+import { SimpleShowLayout } from "@/components/SimpleShowLayout";
+import { Labeled } from "@/components/Labeled";
+import { TextField } from "@/components/TextField";
+import { ReferenceField } from "@/components/ReferenceField";
+
+export const ShowGuesser = (props: { enableLog?: boolean }) => (
+  <ShowBase>
+    <ShowViewGuesser {...props} />
+  </ShowBase>
+);
+
+const ShowViewGuesser = (props: { enableLog?: boolean }) => {
+  const resource = useResourceContext();
+
+  if (!resource) {
+    throw new Error(`Cannot use <ShowGuesser> outside of a ResourceContext`);
+  }
+
+  const { record } = useShowContext();
+  const [child, setChild] = useState<ReactNode>(null);
+  const { enableLog = process.env.NODE_ENV === "development", ...rest } = props;
+
+  useEffect(() => {
+    setChild(null);
+  }, [resource]);
+
+  useEffect(() => {
+    if (record && !child) {
+      const inferredElements = getElementsFromRecords([record], showFieldTypes);
+      const inferredChild = new InferredElement(
+        showFieldTypes.show,
+        null,
+        inferredElements
+      );
+      setChild(inferredChild.getElement());
+
+      if (!enableLog) return;
+
+      const representation = inferredChild.getRepresentation();
+      const components = ["Show"]
+        .concat(
+          Array.from(
+            new Set(
+              Array.from(representation.matchAll(/<([^/\s>]+)/g))
+                .map((match) => match[1])
+                .filter((component) => component !== "span")
+            )
+          )
+        )
+        .sort();
+
+      console.log(
+        `Guessed Show:
+
+${components
+  .map(
+    (component) => `import { ${component} } from "@/components/${component}";`
+  )
+  .join("\n")}
+
+export const ${capitalize(singularize(resource))}Show = () => (
+    <Show>
+${inferredChild.getRepresentation()}
+    </Show>
+);`
+      );
+    }
+  }, [record, child, resource, enableLog]);
+
+  return <ShowView {...rest}>{child}</ShowView>;
+};
+
+const showFieldTypes: InferredTypeMap = {
+  show: {
+    component: (props: any) => <SimpleShowLayout {...props} />,
+    representation: (
+      _props: any,
+      children: { getRepresentation: () => string }[]
+    ) => `        <SimpleShowLayout>
+${children
+  .map((child) => `            ${child.getRepresentation()}`)
+  .join("\n")}
+        </SimpleShowLayout>`,
+  },
+  reference: {
+    component: (props: any) => (
+      <Labeled source={props.source}>
+        <ReferenceField source={props.source} reference={props.reference}>
+          <TextField source="id" />
+        </ReferenceField>
+      </Labeled>
+    ),
+    representation: (props: any) =>
+      `<Labeled source="${props.source}">
+                <ReferenceField source="${props.source}" reference="${props.reference}">
+                    <TextField source="id" />
+                </ReferenceField>
+            </Labeled>`,
+  },
+  string: {
+    component: (props: any) => (
+      <Labeled source={props.source}>
+        <TextField source={props.source} />
+      </Labeled>
+    ),
+    representation: (props: any) =>
+      `<Labeled source="${props.source}">
+                <TextField source="${props.source}" />
+            </Labeled>`,
+  },
+};
--- src/products/index-guessers.tsx
+++ src/products/index-guessers.tsx
@@ -1,8 +1,10 @@
 import { ResourceProps } from "ra-core";
 import { ListGuesser } from "@/components/ListGuesser";
+import { ShowGuesser } from "@/components/ShowGuesser";
 
 export const products: ResourceProps = {
   name: "products",
   list: ListGuesser,
+  show: ShowGuesser,
   recordRepresentation: "reference",
 };
