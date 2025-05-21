c4d29550a67272b21a305021b56253d82aad89c1
Jean-Baptiste Kaiser <jb@marmelab.com>
2025-05-21 09:02:27 +0200

    fix LoginPage on mobile

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -7,7 +7,7 @@ export const LoginPage = () => {
 
   return (
     <div className="min-h-screen flex">
-      <div className="container relative hidden flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <div className="container relative grid flex-col items-center justify-center sm:max-w-none lg:grid-cols-2 lg:px-0">
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
           <div className="absolute inset-0 bg-zinc-900" />
           <div className="relative z-20 flex items-center text-lg font-medium">
