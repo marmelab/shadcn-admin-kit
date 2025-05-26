5cc9f965202492ddb7ce21e581cf02b7a1f6ae7c
fzaninotto <fzaninotto@gmail.com>
2025-05-26 08:10:11 +0200

    Improve pagination

--- src/components/ListPagination.tsx
+++ src/components/ListPagination.tsx
@@ -1,36 +1,169 @@
-import { Button } from "@/components/ui/button";
-import { useListPaginationContext, Translate } from "ra-core";
+import { cn } from "@/lib/utils";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+} from "@/components/ui/pagination";
+import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
+import { useListPaginationContext, useTranslate } from "ra-core";
 
 export const ListPagination = () => {
+  const translate = useTranslate();
   const { hasPreviousPage, hasNextPage, page, perPage, total, setPage } =
     useListPaginationContext();
 
   const pageStart = (page - 1) * perPage + 1;
   const pageEnd = hasNextPage ? page * perPage : total;
 
+  const boundaryCount = 1;
+  const siblingCount = 1;
+  const count = total ? Math.ceil(total / perPage) : 1;
+
+  const range = (start: number, end: number) => {
+    const length = end - start + 1;
+    return Array.from({ length }, (_, i) => start + i);
+  };
+
+  const startPages = range(1, Math.min(boundaryCount, count));
+  const endPages = range(
+    Math.max(count - boundaryCount + 1, boundaryCount + 1),
+    count
+  );
+
+  const siblingsStart = Math.max(
+    Math.min(
+      // Natural start
+      page - siblingCount,
+      // Lower boundary when page is high
+      count - boundaryCount - siblingCount * 2 - 1
+    ),
+    // Greater than startPages
+    boundaryCount + 2
+  );
+
+  const siblingsEnd = Math.min(
+    Math.max(
+      // Natural end
+      page + siblingCount,
+      // Upper boundary when page is low
+      boundaryCount + siblingCount * 2 + 2
+    ),
+    // Less than endPages
+    count - boundaryCount - 1
+  );
+
+  const siblingPages = range(siblingsStart, siblingsEnd);
+
+  const pageChangeHandler = (newPage: number) => {
+    return (event: React.MouseEvent<HTMLAnchorElement>) => {
+      event.preventDefault();
+      setPage(newPage);
+    };
+  };
+
   return (
-    <div className="flex items-center justify-end space-x-2 py-4 px-4">
-      <div className="flex-1 text-sm text-muted-foreground">
+    <div className="flex items-center justify-end space-x-2 py-4  gap-4">
+      <div className="text-sm text-muted-foreground">
         {total != null ? `${pageStart} - ${pageEnd} of ${total}` : null}
       </div>
-      <div className="space-x-2">
-        <Button
-          variant="outline"
-          size="sm"
-          onClick={() => setPage(page - 1)}
-          disabled={!hasPreviousPage}
-        >
-          <Translate i18nKey="ra.navigation.previous">Previous</Translate>
-        </Button>
-        <Button
-          variant="outline"
-          size="sm"
-          onClick={() => setPage(page + 1)}
-          disabled={!hasNextPage}
-        >
-          <Translate i18nKey="ra.navigation.next">Next</Translate>
-        </Button>
-      </div>
+      <Pagination className="-w-full -mx-auto">
+        <PaginationContent>
+          <PaginationItem>
+            <PaginationLink
+              href="#"
+              onClick={pageChangeHandler(page - 1)}
+              className={cn(
+                "gap-1 px-2.5 sm:pr-2.5",
+                !hasPreviousPage ? "opacity-50 cursor-not-allowed" : ""
+              )}
+              aria-label={translate("ra.navigation.previous", {
+                _: "Previous",
+              })}
+            >
+              <ChevronLeftIcon />
+            </PaginationLink>
+          </PaginationItem>
+          {startPages.map((pageNumber) => (
+            <PaginationItem key={pageNumber}>
+              <PaginationLink
+                href="#"
+                onClick={pageChangeHandler(pageNumber)}
+                isActive={pageNumber === page}
+              >
+                {pageNumber}
+              </PaginationLink>
+            </PaginationItem>
+          ))}
+          {siblingsStart > boundaryCount + 2 ? (
+            <PaginationItem>
+              <PaginationEllipsis />
+            </PaginationItem>
+          ) : boundaryCount + 1 < count - boundaryCount ? (
+            <PaginationItem>
+              <PaginationLink
+                href="#"
+                onClick={pageChangeHandler(boundaryCount + 1)}
+                isActive={boundaryCount + 1 === page}
+              >
+                {boundaryCount + 1}
+              </PaginationLink>
+            </PaginationItem>
+          ) : null}
+          {siblingPages.map((pageNumber) => (
+            <PaginationItem key={pageNumber}>
+              <PaginationLink
+                href="#"
+                onClick={pageChangeHandler(pageNumber)}
+                isActive={pageNumber === page}
+              >
+                {pageNumber}
+              </PaginationLink>
+            </PaginationItem>
+          ))}
+          {siblingsEnd < count - boundaryCount - 1 ? (
+            <PaginationItem>
+              <PaginationEllipsis />
+            </PaginationItem>
+          ) : count - boundaryCount > boundaryCount ? (
+            <PaginationItem>
+              <PaginationLink
+                href="#"
+                onClick={pageChangeHandler(count - boundaryCount)}
+                isActive={count - boundaryCount === page}
+              >
+                {count - boundaryCount}
+              </PaginationLink>
+            </PaginationItem>
+          ) : null}
+          {endPages.map((pageNumber) => (
+            <PaginationItem key={pageNumber}>
+              <PaginationLink
+                href="#"
+                onClick={pageChangeHandler(pageNumber)}
+                isActive={pageNumber === page}
+              >
+                {pageNumber}
+              </PaginationLink>
+            </PaginationItem>
+          ))}
+          <PaginationItem>
+            <PaginationLink
+              href="#"
+              onClick={pageChangeHandler(page + 1)}
+              size="default"
+              className={cn(
+                "gap-1 px-2.5 sm:pr-2.5",
+                !hasNextPage ? "opacity-50 cursor-not-allowed" : ""
+              )}
+              aria-label={translate("ra.navigation.next", { _: "Next" })}
+            >
+              <ChevronRightIcon />
+            </PaginationLink>
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
     </div>
   );
 };
