e5d8602a7afe13888f5a8bbb1e85ed64c45e94db
fzaninotto <fzaninotto@gmail.com>
2025-07-01 18:01:58 +0200

    [Demo] Add filters to review list

--- src/reviews/ReviewList.tsx
+++ src/reviews/ReviewList.tsx
@@ -1,13 +1,62 @@
-import { DataTable } from "@/components/admin/data-table";
-import { List } from "@/components/admin/list";
-import { ReferenceField } from "@/components/admin/reference-field";
+import {
+  DataTable,
+  List,
+  ReferenceField,
+  TextInput,
+  ReferenceInput,
+  AutocompleteInput,
+} from "@/components/admin";
 
 import { FullNameField } from "../customers/FullNameField";
 import { StarRatingField } from "./StarRatingField";
 import type { Review } from "../types";
+import { cn } from "@/lib/utils";
+
+const filters = [
+  <TextInput source="q" placeholder="Search" label={false} />,
+  <ReferenceInput
+    source="customer_id"
+    reference="customers"
+    sort={{ field: "last_name", order: "ASC" }}
+  >
+    <AutocompleteInput placeholder="Filter by customer" label={false} />
+  </ReferenceInput>,
+  <ReferenceInput
+    source="product_id"
+    reference="products"
+    sort={{ field: "name", order: "ASC" }}
+  >
+    <AutocompleteInput placeholder="Filter by product" label={false} />
+  </ReferenceInput>,
+  <AutocompleteInput
+    source="status"
+    placeholder="Filter by status"
+    choices={[
+      { id: "accepted", name: "Accepted" },
+      { id: "rejected", name: "Rejected" },
+      { id: "pending", name: "Pending" },
+    ]}
+    optionText={(choice) => (
+      <>
+        <span
+          className={cn(
+            "h-2 w-2 rounded-full",
+            choice.id === "accepted"
+              ? "bg-green-400 dark:bg-green-800"
+              : choice.id === "rejected"
+              ? "bg-red-400 dark:bg-red-800"
+              : "bg-yellow-400 dark:bg-yellow-800"
+          )}
+        />
+        {choice.name}
+      </>
+    )}
+    label={false}
+  />,
+];
 
 export const ReviewList = () => (
-  <List sort={{ field: "date", order: "DESC" }} perPage={25}>
+  <List sort={{ field: "date", order: "DESC" }} perPage={25} filters={filters}>
     <DataTable
       rowClassName={(record: Review) => {
         if (record.status === "accepted") {
