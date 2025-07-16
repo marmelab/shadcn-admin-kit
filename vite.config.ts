5a7316b669724519c5b8ec80d30f789fc36ae598
Jean-Baptiste Kaiser <jb@marmelab.com>
2025-07-16 11:17:14 +0200

    fix demo vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: mode === "production" ? "/demo/" : "./",
-}));
+  base: "./",
+});
