c71529a60d0147194723656da43d591911cbe179
François Zaninotto <fzaninotto@gmail.com>
2025-07-17 19:30:50 +0200

    Remove the notion of click

--- website/src/components/CallToAction.tsx
+++ website/src/components/CallToAction.tsx
@@ -4,9 +4,12 @@ export function CallToAction() {
   return (
     <div className="bg-black">
       <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 text-center lg:px-8">
-        <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl max-w-lg mx-auto">
-          Generate a beautiful admin panel in just a few clicks.
+        <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl max-w-xl mx-auto">
+          Generate a beautiful admin panel in just a few lines of code.
         </h2>
+        <p className="mx-auto mt-6 max-w-2xl text-center text-lg leading-8 text-gray-300">
+          Then customize every detail to fit your unique requirements.
+        </p>
         <Button asChild className="mt-10" size="lg" variant="outline">
           <a
             href="https://github.com/marmelab/shadcn-admin-kit/blob/main/docs/2-Quick-Start-Guide.md"
