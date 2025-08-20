72ea551eda0156ac859b46f310efdfb39a6f6ea9
Matthieu Hochlander <matthieu@marmelab.com>
2025-08-20 17:28:45 +0200

    Merge filter-form and filter-form-input files.

--- src/components/admin/filter-form.tsx
+++ src/components/admin/filter-form.tsx
@@ -1,11 +1,16 @@
 import * as React from "react";
-import get from "lodash/get";
-import { FilterLiveForm, useListContext, useResourceContext } from "ra-core";
 import { type HtmlHTMLAttributes, useCallback, useEffect } from "react";
+import get from "lodash/get";
+import {
+  FilterLiveForm,
+  useListContext,
+  useResourceContext,
+  useTranslate,
+} from "ra-core";
+import { MinusCircle } from "lucide-react";
 import { cn } from "@/lib/utils";
-
 import { FilterElementProps, useFilterContext } from "@/hooks/filter-context";
-import { FilterFormInput } from "./filter-form-input";
+import { Button } from "@/components/ui/button.tsx";
 
 export const FilterForm = (inProps: FilterFormProps) => {
   const { filters: filtersProps, ...rest } = inProps;
@@ -115,3 +120,51 @@ const isEmptyValue = (filterValue: any): boolean => {
 
   return false;
 };
+
+export const FilterFormInput = (inProps: FilterFormInputProps) => {
+  const { filterElement, handleHide, className } = inProps;
+  const resource = useResourceContext(inProps);
+  const translate = useTranslate();
+
+  return (
+    <div
+      data-source={filterElement.props.source}
+      className={cn(
+        "filter-field flex flex-row min-w-64 h-full pointer-events-auto gap-2 relative",
+        "[&_.form-item]:flex-grow [&_.form-item_label]:absolute [&_.form-item_label]:-top-2/3",
+        className,
+      )}
+    >
+      {React.cloneElement(filterElement, {
+        resource,
+        record: emptyRecord,
+        size: filterElement.props.size ?? "small",
+        helperText: false,
+        // ignore defaultValue in Field because it was already set in Form (via mergedInitialValuesWithDefaultValues)
+        defaultValue: undefined,
+      })}
+      {!filterElement.props.alwaysOn && (
+        <Button
+          type="button"
+          variant="ghost"
+          size="sm"
+          className="hide-filter h-9 w-9 cursor-pointer"
+          onClick={handleHide}
+          data-key={filterElement.props.source}
+          title={translate("ra.action.remove_filter")}
+        >
+          <MinusCircle className="h-4 w-4" />
+        </Button>
+      )}
+    </div>
+  );
+};
+
+export interface FilterFormInputProps {
+  filterElement: React.ReactElement<FilterElementProps>;
+  handleHide: (event: React.MouseEvent<HTMLElement>) => void;
+  className?: string;
+  resource?: string;
+}
+
+const emptyRecord = {};
