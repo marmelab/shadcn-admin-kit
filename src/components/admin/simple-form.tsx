1ed389cb533adafb05cc817cd64f76415b568513
fzaninotto <fzaninotto@gmail.com>
2025-08-22 18:23:37 +0200

    Fix SimpleForm syntax

--- src/components/admin/simple-form.tsx
+++ src/components/admin/simple-form.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
 import { Children, ReactNode } from "react";
-import { Form, type FormProps, WithRecord } from "ra-core";
+import { Form, type FormProps } from "ra-core";
 import { cn } from "@/lib/utils";
-import { DeleteButton, CancelButton, SaveButton } from "@/components/admin";
+import { CancelButton, SaveButton } from "@/components/admin";
 
 export const SimpleForm = ({
   children,
@@ -13,41 +13,39 @@ export const SimpleForm = ({
   children: ReactNode;
   className?: string;
   toolbar?: ReactNode;
-} & FormProps) => {
-  return (
-    <Form
-      className={cn(`flex flex-col gap-4 w-full max-w-lg`, className)}
-      {...rest}
-    >
-      {children}
-      {toolbar}
-    </Form>
-  );
-};
+} & FormProps) => (
+  <Form
+    className={cn(`flex flex-col gap-4 w-full max-w-lg`, className)}
+    {...rest}
+  >
+    {children}
+    {toolbar}
+  </Form>
+);
 
-export const FormToolbar = (inProps: FormToolbarProps) => {
-  const { children, className, ...rest } = inProps;
-
-  return (
-    <div
-      {...rest}
-      className={cn(
-        "sticky pt-4 pb-4 md:block md:pt-2 md:pb-0 bottom-0 bg-linear-to-b from-transparent to-background to-10%",
-        className,
-      )}
-      role="toolbar"
-    >
-      {Children.count(children) === 0 ? (
-        <div className="flex flex-row gap-2 justify-end">
-          <CancelButton />
-          <SaveButton />
-        </div>
-      ) : (
-        children
-      )}
-    </div>
-  );
-};
+export const FormToolbar = ({
+  children,
+  className,
+  ...rest
+}: FormToolbarProps) => (
+  <div
+    {...rest}
+    className={cn(
+      "sticky pt-4 pb-4 md:block md:pt-2 md:pb-0 bottom-0 bg-linear-to-b from-transparent to-background to-10%",
+      className,
+    )}
+    role="toolbar"
+  >
+    {Children.count(children) === 0 ? (
+      <div className="flex flex-row gap-2 justify-end">
+        <CancelButton />
+        <SaveButton />
+      </div>
+    ) : (
+      children
+    )}
+  </div>
+);
 
 export interface FormToolbarProps extends React.HTMLAttributes<HTMLDivElement> {
   children?: ReactNode;
