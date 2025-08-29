908847ad18419b8707c8f82f665fe4f34fb7b0a9
François Zaninotto <fzaninotto@gmail.com>
2025-08-29 18:53:06 +0200

    Fix field type

--- src/lib/field.type.ts
+++ src/lib/field.type.ts
@@ -4,7 +4,7 @@ import { ReactNode } from "react";
 export interface FieldProps<
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   RecordType extends Record<string, any> = Record<string, any>,
-> extends BaseFieldProps<RecordType> {
+> extends Omit<BaseFieldProps<RecordType>, 'resource'> {
   /**
    * The component to display when the field value is empty. Defaults to empty string.
    *
