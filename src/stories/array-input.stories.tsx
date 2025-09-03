a5e4019c9428a92b1f4363b1d17181ba4e9494fe
fzaninotto <fzaninotto@gmail.com>
2025-09-03 15:00:38 +0200

    Improve ArrayInput doc

--- src/stories/array-input.stories.tsx
+++ src/stories/array-input.stories.tsx
@@ -1,3 +1,4 @@
+import { ReactNode } from "react";
 import {
   CoreAdminContext,
   minLength,
@@ -5,13 +6,15 @@ import {
   required,
   ResourceContextProvider,
 } from "ra-core";
-import { SimpleForm, TextInput, ThemeProvider } from "@/components/admin";
-import { i18nProvider } from "@/lib/i18nProvider";
-import { ReactNode } from "react";
+import { ThemeProvider } from "@/components/admin/theme-provider";
 import { ArrayInput } from "@/components/admin/array-input";
+import { NumberInput } from "@/components/admin/number-input";
+import { TextInput } from "@/components/admin/text-input";
+import { SimpleForm } from "@/components/admin/simple-form";
 import { SimpleFormIterator } from "@/components/admin/simple-form-iterator";
+import { i18nProvider } from "@/lib/i18nProvider";
 
-const record = {
+const defaultRecord = {
   id: 1,
   tags: [{ name: "tech" }, { name: "news" }, { name: "lifestyle" }],
   title: "My Post",
@@ -29,9 +32,12 @@ export default {
 const StoryWrapper = ({
   children,
   theme,
+  record = defaultRecord,
 }: {
   children: ReactNode;
   theme: "system" | "light" | "dark";
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  record?: any;
 }) => (
   <ThemeProvider defaultTheme={theme}>
     <CoreAdminContext i18nProvider={i18nProvider}>
@@ -66,6 +72,48 @@ export const SeveralInputs = ({
           <SimpleFormIterator>
             <TextInput source="name" />
             <TextInput source="profession" />
+          </SimpleFormIterator>
+        </ArrayInput>
+      </SimpleForm>
+    </ResourceContextProvider>
+  </StoryWrapper>
+);
+
+export const Inline = () => (
+  <StoryWrapper
+    theme="light"
+    record={{
+      id: 1,
+      date: "2022-08-30",
+      customer: "John Doe",
+      items: [
+        {
+          name: "Office Jeans",
+          price: 45.99,
+          quantity: 1,
+        },
+        {
+          name: "Black Elegance Jeans",
+          price: 69.99,
+          quantity: 2,
+        },
+        {
+          name: "Slim Fit Jeans",
+          price: 55.99,
+          quantity: 1,
+        },
+      ],
+    }}
+  >
+    <ResourceContextProvider value="orders">
+      <SimpleForm>
+        <TextInput source="customer" />
+        <TextInput source="date" type="date" />
+        <ArrayInput source="items">
+          <SimpleFormIterator inline>
+            <TextInput source="name" />
+            <NumberInput source="price" />
+            <NumberInput source="quantity" />
           </SimpleFormIterator>
         </ArrayInput>
       </SimpleForm>
