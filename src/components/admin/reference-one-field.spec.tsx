a5b1eb12ae4aa80a217a223133c5956f53c5cfe6
mohndoe <bonjour@doe.cool>
2025-11-03 15:09:45 +0100

    feat(ReferenceOneField): first tests

---
+++ src/components/admin/reference-one-field.spec.tsx
@@ -0,0 +1,17 @@
+import { Basic, WithRenderProp } from "@/stories/reference-one-field.stories";
+import { describe, expect, it } from "vitest";
+import { render } from "vitest-browser-react";
+
+const NOTE_TO_DISPLAY = 'Not very hard. Bit tired after!';
+
+describe('ReferenceOneField', () => {
+    it('should render its child in the context of the related record', async () => {
+        const screen = render(<Basic />);
+        await expect.element(screen.getByText(NOTE_TO_DISPLAY)).toBeInTheDocument();
+    });
+
+    it('should allow to render the referenceRecord using a render prop', async () => {
+        const screen = render(<WithRenderProp />);
+        await expect.element(screen.getByText(NOTE_TO_DISPLAY)).toBeInTheDocument();
+    });
+})
