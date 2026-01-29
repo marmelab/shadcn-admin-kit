29c81875e0ebb35a74cf77650184e21f9977c4c0
Anibal Svarcas <anibal@marmelab.com>
2026-01-29 12:11:34 -0300

    chore: annotate temporary guesser messages

--- src/lib/i18nProvider.ts
+++ src/lib/i18nProvider.ts
@@ -2,6 +2,8 @@ import defaultMessages from "ra-language-english";
 import polyglotI18nProvider from "ra-i18n-polyglot";
 import type { TranslationMessages } from "ra-core";
 
+// TODO: Move these messages to ra-core when available in the next minor release.
+// Remove this override once ra-core ships built-in guesser translations.
 const guesserMessages = {
   guesser: {
     empty: {
