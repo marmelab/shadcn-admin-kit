1cc8f9856e03a8b57788f11eff4a0cbbbd5e68dd
Rin <58572875+TurtIeSocks@users.noreply.github.com>
2026-03-12 16:52:26 -0400

    fix: spooky reduce performance

--- src/components/admin/toggle-filter-button.tsx
+++ src/components/admin/toggle-filter-button.tsx
@@ -65,9 +65,13 @@ const toggleFilter = (value: UnknownRecord, filters: UnknownRecord) => {
   if (isSelected) {
     const keysToRemove = Object.keys(value);
     return Object.keys(filters).reduce(
-      (acc, key) =>
-        keysToRemove.includes(key) ? acc : { ...acc, [key]: filters[key] },
-      {},
+      (acc, key) => {
+        if (!keysToRemove.includes(key)) {
+            acc[key] = filters[key]
+        }
+        return acc
+      },
+      {} as UnknownRecord
     );
   }
 
