24df6e865477b35dad9c1897d1a8a1700cee569a
Raphael Almeida Araujo <raphox.araujo@gmail.com>
2026-06-09 11:39:39 -0300

    Simplify admin Link wrapper

--- src/components/admin/link.tsx
+++ src/components/admin/link.tsx
@@ -1,11 +1,10 @@
 import * as React from "react";
 import { LinkBase, type LinkBaseProps } from "ra-core";
-import { cn } from "@/lib/utils";
 
 /**
  * A router-agnostic Link component for use in admin UI.
  *
- * Wraps ra-core's `LinkBase` with shadcn/ui-compatible styling. This provides a routing
+ * Wraps ra-core's `LinkBase` for admin UI. This provides a routing
  * abstraction layer so that components are not directly coupled to a specific router package.
  *
  * @see {@link https://marmelab.com/shadcn-admin-kit/docs/link/ Link documentation}
@@ -18,12 +17,10 @@ import { cn } from "@/lib/utils";
  * );
  */
 export const Link = React.forwardRef<HTMLAnchorElement, LinkProps>(
-  ({ className, ...props }, ref) => {
-    return <LinkBase ref={ref} className={cn(className)} {...props} />;
+  (props, ref) => {
+    return <LinkBase ref={ref} {...props} />;
   },
 );
 Link.displayName = "Link";
 
-export interface LinkProps extends LinkBaseProps {
-  className?: string;
-}
+export type LinkProps = LinkBaseProps;
